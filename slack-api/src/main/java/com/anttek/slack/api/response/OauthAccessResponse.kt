c9954501eae261557dee5de12a0ed3cc6e6b9377
Bao <leducbao@gmail.com>
2019-10-01 06:42:08 +0200

    complete user and oauth response

--- slack-api/src/main/java/com/anttek/slack/api/response/OauthAccessResponse.kt
+++ slack-api/src/main/java/com/anttek/slack/api/response/OauthAccessResponse.kt
@@ -2,5 +2,25 @@ package com.anttek.slack.api.response
 import com.anttek.slack.api.model.*
 import com.anttek.slack.api.BaseResponse
 
-class OauthAccessResponse (
+class Bot(
+        var bot_user_id: String? = null,
+        var bot_access_token: String? = null
+)
+
+class Team(
+        var id: String? = null,
+        var name: String? = null,
+        var image_48: String? = null
+)
+
+class OauthAccessResponse(
+        var access_token: String? = null,
+        var team: Team? = null,
+        var user: Team? = null,
+        var team_id: String? = null,
+        var team_name: String? = null,
+        var bot: Bot? = null,
+        var user_id: String? = null,
+        var user_name: String? = null
 ): BaseResponse()
+
